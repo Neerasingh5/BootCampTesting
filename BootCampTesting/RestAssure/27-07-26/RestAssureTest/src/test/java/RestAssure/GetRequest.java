45febc96c28b67cd8d2cad2dba0addd563d19b4a
Neerasingh5 <csai23130@glbitm.ac.in>
2026-07-29 05:45:18 +0530

    Add files via upload

---
+++ BootCampTesting/RestAssure/27-07-26/RestAssureTest/src/test/java/RestAssure/GetRequest.java
@@ -0,0 +1,40 @@
+package RestAssure;
+
+			import static io.restassured.RestAssured.*;
+			
+			import io.restassured.response.Response;
+
+			public class GetRequest {
+
+			    public static void main(String[] args) {
+			        Response response =
+			                given()
+			                .when()
+			                    .get("https://dummy.restapiexample.com/api/v1/employees")
+			                .then()
+			                    .extract()
+			                    .response();
+			        System.out.println("Status Code : " + response.getStatusCode());
+			        
+			        
+			        System.out.println("Complete Response : ");
+			        
+			        
+			        System.out.println(response.asPrettyString());
+			        
+			        
+			        System.out.println("Response Body : ");
+			        
+			        
+			        System.out.println(response.getBody().asPrettyString());
+			        
+			        
+			        System.out.println("Status Line : " + response.getStatusLine());
+			        
+			        
+			        System.out.println("Content Type : "+ response.getContentType());
+			        
+			        
+			        System.out.println("Response Time : "+ response.getTime() + " ms");
+			    }
+			}
